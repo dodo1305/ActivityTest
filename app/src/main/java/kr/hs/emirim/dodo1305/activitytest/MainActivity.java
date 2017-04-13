1b8250db33c2247a04534399320b8b1165f530ef
dodo1305 <dodo1305@naver.com>
2017-04-13 20:17:33 +0900

    #3 EventProgramming

--- app/src/main/java/kr/hs/emirim/dodo1305/activitytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dodo1305/activitytest/MainActivity.java
@@ -1,7 +1,10 @@
 package kr.hs.emirim.dodo1305.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butMain = (Button)findViewById(R.id.but_main);
+        butMain.setOnClickListener(butMaininHandler);
     }
+
+    View.OnClickListener butMaininHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            //메소드를 통해서 반환 mainactivity에서 SECOND로 시작한다
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/kr/hs/emirim/dodo1305/activitytest/SecondActivity.java
+++ app/src/main/java/kr/hs/emirim/dodo1305/activitytest/SecondActivity.java
@@ -2,11 +2,22 @@ package kr.hs.emirim.dodo1305.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button butback = (Button)findViewById(R.id.but_second);
+        butback.setOnClickListener(butBackHandler);
     }
+
+    View.OnClickListener butBackHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish(); //현재 액티비티 소멸 secondactivity도 사라짐
+        }
+    };
 }
