1c3c960686dcf994e968c5101f0c94c3375ce747
dodo1305 <dodo1305@naver.com>
2017-04-13 20:51:06 +0900

    #5 UserInSecondActivitiy

--- app/src/main/java/kr/hs/emirim/dodo1305/activitytest/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dodo1305/activitytest/MainActivity.java
@@ -5,22 +5,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+EditText editname, editTel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butMain = (Button)findViewById(R.id.but_main);
         butMain.setOnClickListener(butMaininHandler);
+        editname=(EditText) findViewById(R.id.edit_name);
+        editTel=(EditText) findViewById(R.id.edit_tel);
     }
 
     View.OnClickListener butMaininHandler = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-            //메소드를 통해서 반환 mainactivity에서 SECOND로 시작한다
+            String name = editname.getText().toString();
+            String tel = editname.getText().toString();
+            intent.putExtra("name", name);
+            intent.putExtra("tel", tel);
             startActivity(intent);
         }
     };
